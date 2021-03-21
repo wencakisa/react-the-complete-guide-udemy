2d9c773369f21ee7de73c030bceb36e27d2dbe6f
wencakisa <wencakisa@gmail.com>
2021-03-21 15:21:12 +0200

    [Hooks]: Convert BurgerBuilder to a functional component

--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import _ from 'lodash';
@@ -16,83 +16,78 @@ import BuildControls from '../../components/Burger/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary';
 import { Modal, Spinner } from '../../components/shared';
 
-class BurgerBuilder extends Component {
-  state = {
-    purchasing: false
-  };
+const BurgerBuilder = ({
+  history,
+  ingredients,
+  totalPrice,
+  error,
+  isAuthenticated,
+  initIngredientsHandler,
+  addIngredientHandler,
+  removeIngredientHandler,
+  onInitPurchase,
+  onSetAuthRedirectPath
+}) => {
+  const [purchasing, setPurchasing] = useState(false);
 
-  componentDidMount() {
-    this.props.initIngredientsHandler();
-  }
+  useEffect(() => {
+    initIngredientsHandler();
+  }, []);
 
-  startPurchasingHandler = () => {
-    const { history, isAuthenticated, onSetAuthRedirectPath } = this.props;
-
+  const startPurchasingHandler = () => {
     if (isAuthenticated) {
-      this.setState({ purchasing: true });
+      setPurchasing(true);
     } else {
       onSetAuthRedirectPath('/checkout');
       history.push('/auth');
     }
   };
-  cancelPurchasingHandler = () => this.setState({ purchasing: false });
 
-  goToCheckout = () => {
-    this.props.onInitPurchase();
-    this.props.history.push('/checkout');
+  const cancelPurchasingHandler = () => setPurchasing(false);
+
+  const goToCheckout = () => {
+    onInitPurchase();
+    history.push('/checkout');
   };
 
-  render() {
-    const { purchasing } = this.state;
+  const disabledInfo = _.mapValues(ingredients, value => value <= 0);
+  const purchasable = _.sum(_.values(ingredients)) > 0;
 
-    const {
-      ingredients,
-      totalPrice,
-      error,
-      addIngredientHandler,
-      removeIngredientHandler,
-      isAuthenticated
-    } = this.props;
-
-    const disabledInfo = _.mapValues(ingredients, value => value <= 0);
-    const purchasable = _.sum(_.values(ingredients)) > 0;
-
-    return (
-      <>
-        {!ingredients && (
-          <>
-            {!error && <Spinner />}
-            {error && <p>Ingredients cannot be fetched :(</p>}
-          </>
-        )}
-        {ingredients && (
-          <>
-            <Burger ingredients={ingredients} />
-            <BuildControls
-              addIngredient={addIngredientHandler}
-              removeIngredient={removeIngredientHandler}
-              disabledInfo={disabledInfo}
-              price={totalPrice}
-              purchasable={purchasable}
-              purchase={this.startPurchasingHandler}
-              isAuthenticated={isAuthenticated}
-            />
-            {purchasing && (
-              <Modal onClose={this.cancelPurchasingHandler}>
-                <OrderSummary
-                  ingredients={ingredients}
-                  totalPrice={totalPrice}
-                  continuePurchasing={this.goToCheckout}
-                  cancelPurchasing={this.cancelPurchasingHandler}
-                />
-              </Modal>
-            )}
-          </>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {!ingredients && (
+        <>
+          {!error && <Spinner />}
+          {error && <p>Ingredients cannot be fetched :(</p>}
+        </>
+      )}
+      {ingredients && (
+        <>
+          <Burger ingredients={ingredients} />
+          <BuildControls
+            addIngredient={addIngredientHandler}
+            removeIngredient={removeIngredientHandler}
+            disabledInfo={disabledInfo}
+            price={totalPrice}
+            purchasable={purchasable}
+            purchase={startPurchasingHandler}
+            isAuthenticated={isAuthenticated}
+          />
+          {purchasing && (
+            <Modal onClose={cancelPurchasingHandler}>
+              <OrderSummary
+                ingredients={ingredients}
+                totalPrice={totalPrice}
+                continuePurchasing={goToCheckout}
+                cancelPurchasing={cancelPurchasingHandler}
+              />
+            </Modal>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 const mapStateToProps = ({
   burgerBuilder: { ingredients, totalPrice, error },
