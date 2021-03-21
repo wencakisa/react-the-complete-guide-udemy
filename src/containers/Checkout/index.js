3f30dfdab750557f0363c8167a7a53d54367a69b
wencakisa <wencakisa@gmail.com>
2021-03-21 15:06:48 +0200

    [Hooks]: Convert Checkout to a functional component

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
@@ -6,35 +6,27 @@ import { Route, Redirect } from 'react-router-dom';
 import ContactData from '../Checkout/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary';
 
-class Checkout extends Component {
-  goToHomepage = () => this.props.history.push('/');
-  cancelCheckout = () => this.props.history.goBack();
-  continueCheckout = () => this.props.history.replace('/checkout/contact-data');
+const Checkout = ({ history, match: { path }, ingredients, purchased }) => {
+  const goToHomepage = () => history.push('/');
+  const cancelCheckout = () => history.goBack();
+  const continueCheckout = () => history.replace('/checkout/contact-data');
 
-  render() {
-    const {
-      match: { path },
-      ingredients,
-      purchased
-    } = this.props;
-
-    if (!ingredients) {
-      this.goToHomepage();
-    }
-
-    return (
-      <div>
-        {purchased && <Redirect to="/" />}
-        <CheckoutSummary
-          ingredients={ingredients}
-          onCancel={this.cancelCheckout}
-          onContinue={this.continueCheckout}
-        />
-        <Route path={`${path}/contact-data`} component={ContactData} />
-      </div>
-    );
+  if (!ingredients) {
+    goToHomepage();
   }
-}
+
+  return (
+    <div>
+      {purchased && <Redirect to="/" />}
+      <CheckoutSummary
+        ingredients={ingredients}
+        onCancel={cancelCheckout}
+        onContinue={continueCheckout}
+      />
+      <Route path={`${path}/contact-data`} component={ContactData} />
+    </div>
+  );
+};
 
 const mapStateToProps = ({
   burgerBuilder: { ingredients },
